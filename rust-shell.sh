73b067018daeabd5ea38669318b24febb135cb1d
Ben Nagy <nagy.benb@gmail.com>
2023-10-30 09:54:42 +0000

    feat: initial commit

---
+++ rust-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cargo install
+
+cargo install sccache
+cargo install nu
+cargo install coreutils
+curl -sS https://starship.rs/install.sh | sh
+cargo install ripgrep
+cargo install exa
+cargo install bat
